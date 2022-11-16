ab57612870809e228814a8a182e299360ae0ee10
TeoThabuss <thabussot.teo@proton.me>
2022-11-16 11:52:26 +0100

    added pagination, refactor

--- src/Controller/EtablissementController.php
+++ src/Controller/EtablissementController.php
@@ -4,26 +4,26 @@ namespace App\Controller;
 
 use App\Entity\Etablissement;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EtablissementController extends AbstractController
 {
     #[Route('/etablissement', name: 'app_etablissement')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntityManagerInterface $entityManager, PaginatorInterface $paginator, Request $request): Response
     {
 
-        // TODO Remmetre dans un service
-
-        $etablissements = $entityManager->getRepository(Etablissement::class)->findBy(['estActif' => 1], ["nom" => "ASC"]);
-
-
-
-
+        $pagination = $paginator->paginate(
+            $entityManager->getRepository(Etablissement::class)->findBy(['estActif' => 1], ["nom" => "ASC"]),
+            $request->query->getInt('page', 1), /*page number*/
+            9
+        );
 
         return $this->render('etablissement/index.html.twig', [
-            'etablissements' => $etablissements,
+            'pagination' => $pagination
         ]);
     }
 }
