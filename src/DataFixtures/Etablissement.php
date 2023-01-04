a05bcb07f02daff76b87e7c08c97851c8fee36da
TeoThabuss <thabussot.teo@proton.me>
2023-01-04 09:42:17 +0100

    Ajout des utilisateurs

---
+++ src/DataFixtures/Etablissement.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\DataFixtures;
+include "utils/consts/fixturesConsts.php";
+
+use App\Entity\Categorie;
+use App\Entity\Ville;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class Etablissement extends Fixture
+{
+
+    private SluggerInterface $slugger;
+    private Generator $faker;
+
+    /**
+     * @param SluggerInterface $slugger
+     */
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+        $this->faker = Factory::create('fr_FR');
+    }
+
+
+    public function load(ObjectManager $objectManager): void
+    {
+
+        for ($i = 1;$i <= NOMBRE_DE_ETABLISSEMENT; $i++)
+        {
+
+
+            $villes = $objectManager->getRepository(Ville::class)->findAll();
+            shuffle($villes);
+            $categories = $objectManager->getRepository(Categorie::class)->findAll();
+            shuffle($categories);
+
+            $etablissement = new \App\Entity\Etablissement();
+
+
+            $etablissement->setVille($villes[rand(1, NOMBRE_DE_VILLE - 1)])
+                ->setNom($this->faker->words(3, true))
+                ->setSlug($this->slugger->slug($etablissement->getNom()))
+                ->setDescription($this->faker->paragraph())
+                ->setNumeroTelephone($this->faker->phoneNumber())
+                ->setAdressePostale($this->faker->streetAddress . ', ' . $etablissement->getVille()->getCodePostal() . ', ' . $etablissement->getVille()->getNom())
+                ->setAdresseEmail($this->faker->email())
+                ->setEstActif($this->faker->boolean())
+                ->setEstSurAccueil($this->faker->boolean())
+                ->setCreatedAt($this->faker->dateTimeBetween("-1 week", "-3 day"))
+                ->setUpdatedAt($this->faker->dateTimeBetween("-1 day", "now"))
+                ->addCategorie($categories[rand(0, NOMBRE_DE_CATEGORIE - 1)])
+                ->addCategorie($categories[rand(0, NOMBRE_DE_CATEGORIE - 1)]);
+
+            $objectManager->persist($etablissement);
+
+        }
+
+
+        $objectManager->flush();
+
+    }
+}
---
+++ src/DataFixtures/User.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class User extends Fixture
+{
+
+    private Generator $faker;
+    private UserPasswordHasherInterface $passwordHasher;
+
+
+    public function __construct(UserPasswordHasherInterface $hasher, UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->faker = Factory::create('fr_FR');
+        $this->passwordHasher = $passwordHasher;
+    }
+
+
+    public function load(ObjectManager $objectManager): void
+    {
+
+        for ($i = 1;$i <= NOMBRE_DE_USER; $i++)
+        {
+
+            $user = new \App\Entity\User();
+
+            $user->setEmail($this->faker->email())
+                 ->setPassword($this->passwordHasher->hashPassword($user, "Bonjour25"))
+                 ->setPrenom($this->faker->firstName())
+                 ->setNom($this->faker->lastName())
+                 ->setCreatedAt($this->faker->dateTimeBetween("-3weeks"))
+                 ->setEstActif($this->faker->boolean(80));
+
+
+
+            if ($this->faker->boolean(50)) {
+                $user->setPseudo($this->faker->userName());
+            }
+
+            if ($this->faker->boolean(30)) {
+                $user->setUpdatedAt($this->faker->dateTimeBetween($user->getCreatedAt()));
+            }
+
+
+            if ($this->faker->boolean(80)) {
+
+                if ($this->faker->boolean(10)) {
+
+                    $user->setRoles(["ROLE_ADMIN"]);
+
+                } else {
+
+                    $user->setRoles(["ROLE_USER"]);
+
+                }
+
+            } else {
+
+                $user->setRoles(["ROLE_RESTAURANT"]);
+
+            }
+
+            $objectManager->persist($user);
+
+
+        }
+
+
+        $objectManager->flush();
+
+    }
+}
